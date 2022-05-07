71698b9fba04550d02e3c29abfe385e21456d125
Herman Wong <hwong@hermanwong.net>
2022-05-07 11:23:05 -1000

    Working modules example.

--- prod/data-stores/mysql/outputs.tf
+++ prod/data-stores/mysql/outputs.tf
@@ -1,8 +1,8 @@
 output "address" {
-  value       = aws_db_instance.example2.address
+  value       = aws_db_instance.prod-example.address
   description = "Connect to the database at this endpoint"
 }
 output "port" {
-  value       = aws_db_instance.example2.port
+  value       = aws_db_instance.prod-example.port
   description = "The port the database is listening on."
 }
