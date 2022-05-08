24cdc5d728352a4da14b0c3c9b9f6966584c9b3b
Herman Wong <hwong@hermanwong.net>
2022-05-08 11:38:50 -1000

    Move modules back to local source.

--- live/stage/services/webserver-cluster/main.tf
+++ live/stage/services/webserver-cluster/main.tf
@@ -14,12 +14,14 @@ terraform {
 }
 
 module "webserver_cluster" {
-  source                 = "../../../modules/services/webserver-cluster"
+  source                 = "../../../../modules/services/webserver-cluster"
+  
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "terraform-up-and-running-state-4ecd0688"
   db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
 
-  instance_type = "t2.micro"
-  min_size      = 2
-  max_size      = 2
+  instance_type      = "t2.micro"
+  min_size           = 2
+  max_size           = 2
+  enable_autoscaling = false
 }
